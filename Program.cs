b2ca8ed3e38afffb055243c6eb1839bc832eb415
Oleg <barsuk2000@yandex.ru>
2023-03-18 09:23:50 +0300

    Project get new function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+string? name;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Git test!");
+Console.Write("Enter your name: ");
+name = Console.ReadLine();
+
+Console.WriteLine("Your name: {0}", name);
